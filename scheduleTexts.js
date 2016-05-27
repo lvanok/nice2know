5e15f83bc8ffcaaad5f9bd794ceaf73e90b5fa2b
rparker24 <robert@robertmparker.net>
2016-05-27 13:20:24 -0400

    updated send texts file

--- scheduleTexts.js
+++ scheduleTexts.js
@@ -8,23 +8,31 @@ var Category = require('../models/Category.js');
 var texter = require('../config/sms_message');
 var passwords = require('../config/passwords');
 
-// grab user_id's for all users who are subscribed to any category
-sequelize.query('SELECT DISTINCT user_id FROM user_categories;').then(function(subUsers) {
-  console.log(subUsers);
-  // wrap function in a for loop to run for each user who is signed up to receive texts
-  for (var i = 0; i < subUsers.length; i++) {
-    sequelize.sequelize.query('SELECT * FROM facts INNER JOIN user_categories ON facts.category_id=user_categories.category_id AND user_categories.user_id=' + subUsers[i].user_id + ';').then(function(facts) {
-        var randNum = Math.floor(Math.random() * facts.length);
+function sendTexts() {
+    // grab user_id for all users who are subscribed to any category
+    sequelize.query('SELECT DISTINCT user_id FROM user_categories;').then(function(subUsers) {
+      console.log(subUsers);
+      // wrap function in a for loop to run for each user who is signed up to receive texts
+        for (var i = 0; i < subUsers.length; i++) {
+            sequelize.sequelize.query('SELECT * FROM facts INNER JOIN user_categories ON facts.category_id=user_categories.category_id AND user_categories.user_id=' + subUsers[i].user_id + ';').then(function(facts) {
+                var randNum = Math.floor(Math.random() * facts.length);
 
-        console.log(facts[0][randNum].fact);
-        var fixPhone = subUsers[i].phone.replace('-', '');
-        var fullPhone = '+' + subUsers[i].countrycode + fixPhone;
-        texter.sendMessage({
+                console.log(facts[0][randNum].fact);
 
-            to: fullPhone,  // "+" + req.session.countrycode + req.session.phone,
-            from: passwords.twilioNumber,
-            body: facts[0][randNum].fact //result[randomNum]
-        });
+                sequelize.query('SELECT countrycode, phone FROM users WHERE user_id =' + subUsers[i].user_id + ';').then(function(data) {
+
+                    var fixPhone = data.phone.replace('-', '');
+                    var fullPhone = '+' + data.countrycode + fixPhone;
+                    texter.sendMessage({
+
+                        to: fullPhone,  // "+" + req.session.countrycode + req.session.phone,
+                        from: passwords.twilioNumber,
+                        body: facts[0][randNum].fact //result[randomNum]
+                    });
+                });
+            });
+        }
     });
-  }
-});
+};
+
+sendTexts();
